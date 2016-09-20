02a824f48e9c33a4f7b3bef993a2ca8dbebc89ff
Phil Crooks <pacman@crooks.me.uk>
2016-09-20 19:32:30 +0100

    minor optimisatin to ProductOffer

--- shopping_basket/product_offer.js
+++ shopping_basket/product_offer.js
@@ -3,14 +3,18 @@ function ProductOffer(id, noItems, discount) {
   var mNoItems = noItems;
   var mDiscount = discount;
 
+  function calculatePriceOf(noItems, priceEach) {
+    var discountApplyNumber = Math.floor(noItems/mNoItems) * mNoItems;
+    var discountDoesNotApplyNumber = noItems - discountApplyNumber;
+    var cost = (discountApplyNumber * priceEach);
+    cost -= (cost * mDiscount) / 100.0;
+    cost += discountDoesNotApplyNumber * priceEach;
+    return Math.round(cost * 100) / 100;
+  }
+
   return {
     priceOf: function(noItems, priceEach) {
-      var discountApplyNumber = Math.floor(noItems/mNoItems) * mNoItems;
-      var discountDoesNotApplyNumber = noItems - discountApplyNumber;
-      var cost = (discountApplyNumber * priceEach);
-      cost -= (cost * mDiscount) / 100.0;
-      cost += discountDoesNotApplyNumber * priceEach;
-      return Math.round(cost * 100) / 100;
+      return calculatePriceOf(noItems, priceEach);
     }
   }
 }
